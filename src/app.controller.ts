a02d489835ded00445f8956abc471fc3cb266d46
Timur Samatov <cooool.loki@gmail.com>
2026-03-13 17:24:52 -0500

    Separated and configured artillery test setups for fast and slow endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("slow")
+  async getSlow(): Promise<string> {
+    await new Promise((resolve) => setTimeout(resolve, 4));
+    return this.appService.getSlow();
+  }
 }
